5c19b8ae7bde85b71e35c1fcd3ed9f400708479a
ryanbarrett1515 <barrettra@g.cofc.edu>
2018-06-23 23:30:15 -0400

    created parts of gui

---
+++ SquidParametersManagerApp/src/main/java/org/cirdles/squidparametersmanager/SquidParametersManager.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cirdles.squidparametersmanager;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+
+
+/**
+ *
+ * @author ryanb
+ */
+public class SquidParametersManager extends Application{
+
+    public static Window primaryStageWindow;
+    public static Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(800);
+        
+        Parent root = new AnchorPane();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("SquidParametersManagerGUI.fxml")));
+        
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Squid Parameters Manager");
+
+        primaryStageWindow = primaryStage.getScene().getWindow();
+
+        primaryStage.setOnCloseRequest((WindowEvent e) -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
